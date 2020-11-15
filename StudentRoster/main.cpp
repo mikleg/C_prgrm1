c98ea7101daaef659827d24d5913ca3ed887bcbd
mikle <mihailgorbunov74@gmail.com>
2020-11-15 13:55:46 -0800

    Student class

--- StudentRoster/main.cpp
+++ StudentRoster/main.cpp
@@ -1,7 +1,7 @@
 // StudentRoster.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+//using namespace std;
 
 int main()
 {
--- StudentRoster/student.h
+++ StudentRoster/student.h
@@ -1,5 +1,17 @@
 #pragma once
-class Student
-{
+using namespace std;
+#include <string>
+#include "degree.h"
+class Student	{
+	private:
+		static const short MAX_CLASSES = 100;
+		string studentID;
+		string firstName;
+		string lastName;
+		string email;
+		unsigned short age;
+		int numberDaysPerClass[MAX_CLASSES];
+		DegreeProgram degreeProgram;
+		
 };
 
