f8aceca2548c626e283ad92afd04bf5b5cf61089
mikle <mihailgorbunov74@gmail.com>
2020-11-24 13:44:31 -0800

    Roster add was changed using new operator

--- StudentRoster/roster.cpp
+++ StudentRoster/roster.cpp
@@ -4,7 +4,23 @@ Roster::Roster() {
 }
 void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age,
 	int daysInCourse[], int numberCorses, DegreeProgram degreeprogram) {
-	Student instanceStudent(studentID, firstName, lastName, emailAddress, age, daysInCourse, numberCorses, degreeprogram);
-	this->classRosterArray[currentNumStudents] = &instanceStudent;
-	currentNumStudents += 1;
+	//Student instanceStudent(studentID, firstName, lastName, emailAddress, age, daysInCourse, numberCorses, degreeprogram);
+	//this->classRosterArray[currentNumStudents] = &instanceStudent;
+		//Student instanceStudent(studentID, firstName, lastName, emailAddress, age, daysInCourse, numberCorses, degreeprogram);
+	this->classRosterArray[currentNumStudents] = new Student(studentID, firstName, lastName, emailAddress, age, daysInCourse, numberCorses, degreeprogram);
+	currentNumStudents ++;
+}
+void Roster::remove(string studentID) {
+	for (int i = 0; i < currentNumStudents; i++) {
+		// get shallow copy of actual object
+		Student tempInstanceStudent = *classRosterArray[i];
+		if (studentID == tempInstanceStudent.getStudentID()) {
+			//delete actual object. 
+			delete classRosterArray[i];
+
+
+
+
+		}
+	}
 }
