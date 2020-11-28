83379fb7e418298a76fc31be74158a91cb400c2b
mikle <mihailgorbunov74@gmail.com>
2020-11-27 20:51:22 -0800

    a lttle bit more :-)

--- StudentRoster/main.cpp
+++ StudentRoster/main.cpp
@@ -3,15 +3,14 @@ using namespace std;
 #include <string>
 #include "roster.h"
 
-int main()
-{   
+int main(){   
     const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
                                   "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
                                   "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
                                   "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
                                   "A5,Mikhail,Gorbunov,mihailgorbunov74@gmail.com,43,30,30,30,SOFTWARE"
                                   
-    };
+                                 };
     Roster *classRoster = new Roster(studentData, 5);
     classRoster->printAll();
     classRoster->printInvalidEmails();
